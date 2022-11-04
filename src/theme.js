59d462441756039de6dd4423c9d057eb7a24b615
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-04 13:42:55 +0300

    Added function in Context for changing color theme of app

--- src/theme.js
+++ src/theme.js
@@ -163,32 +163,53 @@ export const themeSettings = (mode) => {
           }),
     },
     typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
         fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-        fontSize: 12,
-        h1: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 40
-        },
-        h2: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 32
-        },
-        h3: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 24
-        },
-        h4: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 20
-        },
-        h5: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 16
-        },
-        h6: {
-            fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
-            fontSize: 14
-        },
-    }
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
   };
+};
+
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
 };
