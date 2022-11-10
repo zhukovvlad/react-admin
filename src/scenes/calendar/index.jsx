52e879e792190bd09eaf1566148017908a342758
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-10 20:48:21 +0300

    Started calendar component

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -16,6 +16,16 @@ import Header from "../../components/Header";
 import { tokens } from "../../theme";
 
 const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("Please enter new title for your event");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+  }
+
   return <div>Calendar</div>;
 };
 
