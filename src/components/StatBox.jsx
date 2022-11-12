bd7c0b625548782232752da2cb83524fed91de8a
zhukovvlad <zhukovvlad@yandex.ru>
2022-11-12 15:51:13 +0300

    Started Dashboard page filling

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -18,22 +18,22 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
           >
             {title}
           </Typography>
-          <Box>
-            <ProgressCircle progress={progress} />
-          </Box>
-          <Box display="flex" justifyContent="space-between">
-            <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
-              {subtitle}
-            </Typography>
-            <Typography
-              variant="h5"
-              fontStyle="italic"
-              sx={{ color: colors.greenAccent[600] }}
-            >
-              {increase}
-            </Typography>
-          </Box>
         </Box>
+        <Box>
+          <ProgressCircle progress={progress} />
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,12 +1,88 @@
 import React from "react";
 import Header from "../../components/Header";
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/Barchart";
+import GeographyChart from "../../components/GeographyChart";
+import PieChart from "../../components/PieChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
 
 function Dashboard() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* Row 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="435,567"
+            subtitle="Sales Obtained"
+            progress="0.5"
+            increase="+21%"
+            icon={
+              <PointOfSaleIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
       </Box>
     </Box>
   );
